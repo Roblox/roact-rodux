beee598cba8fbba478fb3724f77cc0bdc33c7e57
Lucien Greathouse <me@lpghatguy.com>
2018-04-10 17:20:30 -0700

    Fix case that was missing props

--- lib/connect2.lua
+++ lib/connect2.lua
@@ -67,7 +67,7 @@ local function connect2(mapStateToProps, mapDispatchToProps)
 
 			local stateMapper = mapStateToProps
 
-			local stateValues = mapStateToProps(storeState)
+			local stateValues = mapStateToProps(storeState, self.props)
 			if typeof(stateValues) == "function" then
 				stateMapper = stateValues
 				stateValues = stateValues(storeState)
