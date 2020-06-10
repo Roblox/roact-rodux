1c8bd5a1b6f05af23f4e4955ab2286a3ed475db0
Henry Allen <hallen@roblox.com>
2020-06-10 15:45:55 -0700

    Issue-26 updated StoreProvider to be compliant with Context API update

--- src/StoreProvider.lua
+++ src/StoreProvider.lua
@@ -4,18 +4,28 @@ local storeKey = require(script.Parent.storeKey)
 
 local StoreProvider = Roact.Component:extend("StoreProvider")
 
-function StoreProvider:init(props)
+local StoreContext = Roact.createContext()
+
+function StoreProvider.validateProps(props)
 	local store = props.store
-
+	
 	if store == nil then
-		error("Error initializing StoreProvider. Expected a `store` prop to be a Rodux store.")
+		return false, "Error initializing StoreProvider. Expected a `store` prop to be a Rodux store."
+	else
+		return true
 	end
+end
 
-	self._context[storeKey] = store
+function StoreProvider:init(props)
+	self.store = props.store
+	self._context[storeKey] = props.store
 end
 
 function StoreProvider:render()
-	return Roact.oneChild(self.props[Roact.Children])
+	return Roact.createElement(StoreContext.Provider, {
+		value = self.store
+	}, self.props[Roact.Children])
 end
 
+--Switch to function component wrapper, pass in props.store, use it is as value for context provider 
 return StoreProvider
