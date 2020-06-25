e51ad89f01289c14aca9de14f8fa49333b0b5230
hallenrbx <66646616+hallenrbx@users.noreply.github.com>
2020-06-25 12:30:04 -0700

    Issue 27 Support multiple children for StoreProvider (#35)
    * Issue 27 Support multiple children for StoreProvider
    
    Issue: update StoreProvider:Render() to support fragments, replacing
    Roact.oneChild
    
    * Issue 27: Added unit test
    
    * Updated StoreProvider Multiple Children Test so that it can't just return nil in the render function and still pass
    
    * Issue 27, updated tests to check Roblox Dom to make sure that child elements are actually rendered
    
    * Fixed test for StoreProvider by moving Folder instance creation inside the test
    
    * Updated Accept Multiple Children test to be more in line with other lua tests

--- src/StoreProvider.spec.lua
+++ src/StoreProvider.spec.lua
@@ -3,6 +3,7 @@ return function()
 
 	local Roact = require(script.Parent.Parent.Roact)
 	local Rodux = require(script.Parent.Parent.Rodux)
+	
 
 	it("should be instantiable as a component", function()
 		local store = Rodux.Store.new(function()
@@ -26,5 +27,32 @@ return function()
 		expect(function()
 			Roact.mount(element)
 		end).to.throw()
+	end)
+
+	it("should accept multiple children", function()
+		local store = Rodux.Store.new(function()
+			return 0
+		end)
+
+		local folder = Instance.new("Folder")
+
+		local element = Roact.createElement(StoreProvider, {
+			store = store
+		}, {
+			test1 = Roact.createElement("Frame"),
+			test2 = Roact.createElement("Frame"),
+			test3 = Roact.createElement("Frame")
+		})
+
+		expect(element).to.be.ok()
+
+		local handle = Roact.mount(element, folder, "StoreProvider-test")
+
+		expect(folder:FindFirstChild("test1")).to.be.ok()
+		expect(folder:FindFirstChild("test2")).to.be.ok()
+		expect(folder:FindFirstChild("test3")).to.be.ok()
+
+		Roact.unmount(handle)
+		store:destruct()
 	end)
 end
