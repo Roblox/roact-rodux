c83dfae3dca1c0593f9ee132add239f9491e6703
Matt Hargett <plaztiksyke@gmail.com>
2021-11-02 15:53:35 -0700

    Adjust upstream LICENSE location wording

--- src/ReactSymbols.lua
+++ src/ReactSymbols.lua
@@ -4,7 +4,7 @@
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * LICENSE file at https://github.com/facebook/react/blob/a774502e0ff2a82e3c0a3102534dbc3f1406e5ea/LICENSE
  *
  * @flow
  ]]
