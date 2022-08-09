1011b33c4321ba53c15bcbed259a3d49f65c6433
jkelaty <jkelaty@roblox.com>
2022-08-08 18:28:28 -0700

    Add action/thunk prop types

---
+++ src/types.lua
@@ -0,0 +1,11 @@
+--!strict
+local Rodux = require(script.Parent.Parent.Rodux)
+
+type BaseAction = Rodux.Action<string>
+type ThunkAction<ReturnType, State> = Rodux.ThunkAction<ReturnType, State>
+
+export type DispatchProp = <Action>(action: Action & BaseAction) -> ()
+
+export type ThunkfulDispatchProp<State = any> = DispatchProp & <ReturnType>(thunkAction: ThunkAction<ReturnType, State>) -> ReturnType
+
+return nil
