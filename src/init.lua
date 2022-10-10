a22d98bf975565dceafa21d27f0322942f902c05
jkelaty-rbx <78873527+jkelaty-rbx@users.noreply.github.com>
2022-10-10 14:12:44 -0700

    LUAFDN-250 - Add types for connect and dispatch prop (#68)
    This PR adds types for the dispatch prop and connect. This includes a standard action dispatch and a thunkful action dispatch variant.
    
    Note that I used the thunkful action dispatch as the default for connect(), which may or may not be desirable, but felt like a better default to me.
    
    Inspired by: https://github.com/reduxjs/react-redux/blob/master/src/types.ts
    
    This RP also adds typechecking to CI with luau-lsp.

--- src/init.lua
+++ src/init.lua
@@ -1,6 +1,11 @@
+--!strict
 local StoreProvider = require(script.StoreProvider)
 local StoreContext = require(script.StoreContext)
 local connect = require(script.connect)
+local types = require(script.types)
+
+export type DispatchProp = types.DispatchProp
+export type ThunkfulDispatchProp<State = any> = types.ThunkfulDispatchProp<State>
 
 return {
 	StoreProvider = StoreProvider,
